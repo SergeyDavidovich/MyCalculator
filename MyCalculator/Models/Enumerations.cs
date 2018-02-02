3f393e1d1d394f1902762c89735eb3908a585046
Sergey Krivobokov <writesd@hotmail.com>
2018-02-02 15:45:10 +0100

    Добавьте файлы проекта.

---
+++ MyCalculator/Models/Enumerations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    /// <summary>
+    /// Перечесление символов операций 
+    /// </summary>
+    public enum Operations
+    {
+        Plus,
+        Multiplication,
+        Division,
+        Minus,
+        Discount
+    }
+}
